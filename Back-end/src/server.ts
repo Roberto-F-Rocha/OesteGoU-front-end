7501fb0c450d0481247d26086061672d1a69ebcc
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2026-05-01 19:42:30 -0300

    Enable audit middleware

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -2,11 +2,15 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import { auditLogger } from "./middlewares/auditLogger";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// 🔥 AUDITORIA GLOBAL
+app.use(auditLogger);
 
 app.use(routes);
 
