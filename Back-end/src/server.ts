5a896ea9488b09c168dc69fb7214f623a0d41abe
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2026-05-01 20:21:53 -0300

    Apply security, sanitize and LGPD

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -4,12 +4,21 @@ import cors from "cors";
 import routes from "./routes";
 import { auditLogger } from "./middlewares/auditLogger";
 import { errorHandler } from "./middlewares/errorHandler";
+import { securityMiddleware } from "./middlewares/security";
+import { sanitizeInput } from "./middlewares/sanitize";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// 🔒 SEGURANÇA
+app.use(securityMiddleware);
+
+// 🧼 SANITIZAÇÃO
+app.use(sanitizeInput);
+
+// 🧾 LOG
 app.use(auditLogger);
 
 app.use(routes);
