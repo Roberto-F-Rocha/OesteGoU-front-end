45c420ed016ef13e96432a631b28dfce04911870
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2026-05-02 15:57:23 -0300

    fix: use correct token for websocket auth

--- Front-end/src/services/socket.ts
+++ Front-end/src/services/socket.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 export const socket = io("http://localhost:3001", {
   auth: {
-    token: localStorage.getItem("token"),
+    token: localStorage.getItem("oestegou:accessToken"),
   },
 });
